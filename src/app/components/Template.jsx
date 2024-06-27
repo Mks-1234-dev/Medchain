01007d7e94cecbd81614a115ce89c26dacbc791f
Mks-1234-dev <seemasatya2@gmail.com>
2024-06-27 18:59:16 -0500

    Create Template.jsx

---
+++ src/app/components/Template.jsx
@@ -0,0 +1,18 @@
+import React, { useContext, useRef, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+function FrozenRouter(props) {
+  const context = useContext(LayoutRouterContext ?? {});
+  const frozen = useRef(context).current;
+
+  return (
+    <LayoutRouterContext.Provider value={frozen}>
+      {props.children}
+    </LayoutRouterContext.Provider>
+  );
+}
+
+export default function Template({ children }) {
+  useEffect(() => {}, []);
