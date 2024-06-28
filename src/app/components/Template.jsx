d764b94046df2036a856f2a66ffdfcd15f64b2e1
Mks-1234-dev <seemasatya2@gmail.com>
2024-06-28 10:29:42 +0530

    Update Template.jsx

--- src/app/components/Template.jsx
+++ src/app/components/Template.jsx
@@ -16,3 +16,22 @@ function FrozenRouter(props) {
 
 export default function Template({ children }) {
   useEffect(() => {}, []);
+
+  let pathname = usePathname();
+
+  return (
+    <>
+      <AnimatePresence mode={"wait"} initial={false}>
+        <motion.div
+          key={pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3, ease: [0.27, 0.94, 0.48, 1.0] }}
+        >
+          <FrozenRouter>{children}</FrozenRouter>
+        </motion.div>
+      </AnimatePresence>
+    </>
+  );
+}
