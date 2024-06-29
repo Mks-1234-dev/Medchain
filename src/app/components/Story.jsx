e7a1c37d75d5178bc0ed228a0fbb7b7284e4dc41
Mks-1234-dev <seemasatya2@gmail.com>
2024-06-29 17:10:07 +0530

    first commit120

--- src/app/components/Story.jsx
+++ src/app/components/Story.jsx
@@ -39,31 +39,12 @@ const Story = () => {
           <h1 className="text-4xl mb-4">Story</h1>
           <div className="max-w-xl">
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque,
-              sapiente! Suscipit temporibus fugit ab voluptas molestiae
-              voluptate nihil enim quis ducimus, exercitationem distinctio amet
-              iste culpa ad natus beatae ut?Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Placeat nisi mollitia porro quam
-              dolorum obcaecati at odit sequi, adipisci, delectus laudantium
-              cupiditate repudiandae? Relopellat iste provident natus odio a
-              quam?Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptas, nisi perspiciatis similique consectetur vel dolore
-              laborum alias voluptates distinctio, atque explicabo quidem, a
-              commodi dolorum. Sed deleniti laudantium itaque iusto Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Delectus fugiat
-              numquam voluptatum, dolore laboriosam aliquam facere modi
-              repellat? Est ex veritatis sed vero nobis nemo voluptates nam amet
-              numquam quis? Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Neque fugiat fugit tempora, tempore facere, ut iure non
-              libero quisquam sint exercitationem aliquam enim. Aspernatur sed
-              impedit dolor vero sit reprehenderit? Lorem ipsum dolor sit amet,
-              consectetur adipisicing elit. Maxime fuga impedit exercitationem
-              dolores in labore. Beatae officia tempore accusantium esse
-              quaerat, temporibus expedita id omnis, reprehenderit reiciendis
-              fuga quae aspernatur. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Omnis dolor tempora molestiae ut doloribus
-              aliquid recusandae, voluptates ex error enim quam magnam qui
-              necessitatibus similique mollitia nobis assumenda nesciunt porro!
+              Sabyasachi Mohanty is a visionary Innovator and a trailblazer in
+              integrating data science with healthcare. Having dedicated his
+              academic and professional journey to Healthcare, Sabyasachi spent
+              several years at prominent research institutions, where he focused
+              on healthcare data analysis and machine learning applications in
+              systems biology.
             </p>
           </div>
         </div>
